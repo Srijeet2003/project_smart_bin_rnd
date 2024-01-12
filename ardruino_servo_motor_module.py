6d8eb9d8769658daa875c85bcda5e23745817333
s0um0r0y <soumoroy09@gmail.com>
2024-01-12 09:22:54 +0530

    added code for drawing a contour box

--- ardruino_servo_motor_module.py
+++ ardruino_servo_motor_module.py
@@ -40,9 +40,9 @@ while ret:
         x1,y1,x2,y2,score,class_id=result
 
         if score>threshold:
-            cv2.rectangle(frame,(int(x1),int(y1)),(int(x2),int(y2)),(0, 255, 0), 4)
-            cv2.putText(frame,results.names[int(class_id)].upper(), (int(x1), int(y1 - 10)),
-                        cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3, cv2.LINE_AA)
+            cv2.rectangle(frame,(int(x1),int(y1)),(int(x2),int(y2)),(0,255,0),4)
+            cv2.putText(frame,results.names[int(class_id)].upper(),(int(x1),int(y1-10)),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1.3,(0,255,0),3,cv2.LINE_AA)
 
     out.write(frame)
     ret,frame=cap.read()
